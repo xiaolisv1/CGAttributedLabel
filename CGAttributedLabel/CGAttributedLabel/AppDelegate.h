a7388d588635e30d59b2f25d1cd2b9300263ed81
lichuanguang <674119182@qq.com>
2017-08-29 10:01:29 +0800

    提交信息

---
+++ CGAttributedLabel/CGAttributedLabel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CGAttributedLabel
+//
+//  Created by xiao li on 2017/8/29.
+//  Copyright © 2017年 xiao li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CGAttributedLabel/CGAttributedLabel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CGAttributedLabel
+//
+//  Created by xiao li on 2017/8/29.
+//  Copyright © 2017年 xiao li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
